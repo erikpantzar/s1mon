20b2b27d23cf86b9c27dc4a899efec874f4908fb
epantzar <erxz0r@gmail.com>
2016-09-19 21:13:51 +0200

    Update with h1scores

---
+++ src/hiscores.js
@@ -0,0 +1,37 @@
+import 'whatwg-fetch';
+
+const API = 'https://h1score.herokuapp.com/api';
+//const API = "http://localhost:8080/api";
+
+let hiscoreContainer = document.createElement('section');
+hiscoreContainer.classList.add('hiscores');
+document.body.appendChild(hiscoreContainer);
+
+const hiscore = {
+    getScores: ()=> {
+        console.log('getting scores');
+        fetch(`${API}/h1score`)
+            .then((res)=> res.json())
+            .then((hiscores)=> {
+                hiscoreContainer.innerHTML = "";
+                hiscore.scoreHandler(hiscores);
+            });
+    },
+    scoreHandler: (scores)=> {
+        console.log('writing scores to dom');
+        scores.forEach((score)=> {
+            let item = document.createElement('div');
+            item.classList.add('hiscores-score');
+            item.innerHTML = `
+                       <span class="name">${score.name}</span> <span class="result">${score.score}</span>
+      <span class="time">${score.time}</span>
+          `;
+            hiscoreContainer.appendChild(item);
+        })
+    }
+};
+
+setInterval(hiscore.getScores, 25000);
+hiscore.getScores();
+
+export default hiscore;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import simon from './simon';
-import 'whatwg-fetch';
-
-
-const API = 'https://h1score.herokuapp.com/api'
-
-fetch(`${API}/h1score`)
-   .then((response)=>{
-           console.log('response', response);
- });
-
+import hiscores from './hiscores';
 
 simon.init();
 simon.startGame();
+
+
